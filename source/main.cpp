d3900ca5ef425653b17b7d7b26dd3f10f31c55ad
Danny de Bruijne <dannydb44@gmail.com>
2022-02-25 17:13:59 +0000

    Implement tray cpp port

--- source/main.cpp
+++ source/main.cpp
@@ -1,87 +1,50 @@
-#include <stdio.h>
-#include <string.h>
-#include "defines.h"
-#include "tray.h"
+#include "tray.hpp"
+#include <iostream>
 
-using namespace std;
+using namespace Tray;
 
-extern struct tray tray;
+std::vector<TrayMenu*> menu;
+TrayIcon tr = { "icon.png", "icon.ico", "Phalanx Tray App", menu };
+TrayMenu comPorts = { "Serial Port", true, false, false, nullptr };
 
-static void toggle_cb(struct tray_menu *item) {
-  printf("toggle cb\n");
-  item->checked = !item->checked;
-  tray_update(&tray);
-}
-
-static void hello_cb(struct tray_menu *item) {
-  (void)item;
-  printf("hello cb\n");
-  if (strcmp(tray.icon, TRAY_ICON1) == 0) {
-    tray.icon = TRAY_ICON2;
-  } else {
-    tray.icon = TRAY_ICON1;
-  }
-  tray_update(&tray);
-}
-
-static void quit_cb(struct tray_menu *item) {
-  (void)item;
-  printf("quit cb\n");
-  tray_exit();
-}
-
-static void submenu_cb(struct tray_menu *item) {
-  (void)item;
-  printf("submenu: clicked on %s\n", item->text);
-  tray_update(&tray);
-}
-
-struct tray tray = {
-    .icon = TRAY_ICON1,
-#if TRAY_WINAPI
-    .tooltip = "Tray",
-#endif
-    .menu =
-        (struct tray_menu[]) {
-            {.text = "Hello", .cb = hello_cb},
-            {.text = "Checked", .checked = 1, .checkbox = 1, .cb = toggle_cb},
-            {.text = "Disabled", .disabled = 1},
-            {.text = "-"},
-            {.text = "SubMenu",
-             .submenu =
-                 (struct tray_menu[]) {
-                     {.text = "FIRST", .checked = 1, .checkbox = 1, .cb = submenu_cb},
-                     {.text = "SECOND",
-                      .submenu =
-                          (struct tray_menu[]) {
-                              {.text = "THIRD",
-                               .submenu =
-                                   (struct tray_menu[]) {
-                                       {.text = "7", .cb = submenu_cb},
-                                       {.text = "-"},
-                                       {.text = "8", .cb = submenu_cb},
-                                       {.text = NULL}}},
-                              {.text = "FOUR",
-                               .submenu =
-                                   (struct tray_menu[]) {
-                                       {.text = "5", .cb = submenu_cb},
-                                       {.text = "6", .cb = submenu_cb},
-                                       {.text = NULL}}},
-                              {.text = NULL}}},
-                     {.text = NULL}}},
-            {.text = "-"},
-            {.text = "Quit", .cb = quit_cb},
-            {.text = NULL}},
-};
+std::function changeModel = [&](TrayMenu* tm){ };
 
 int main()
 {
-  if (tray_init(&tray) < 0) {
-    printf("failed to create tray\n");
-    return 1;
-  }
-  while (tray_loop(1) == 0) {
-    printf("iteration\n");
-  }
-  return 0;
+	TrayMenu comPorts = { "Serial Port", true, false, false, nullptr };
+	for(int i = 0; i < 5; i++) {
+		std::string comname = "COM";
+		comname += (char)(i + '0');
+		comPorts.subMenu.push_back(new TrayMenu { comname, true, false, false, [&](TrayMenu* tm){ 
+			for (TrayMenu* t : comPorts.subMenu)
+			{
+				t->isChecked = false;
+			}
+
+			tm->isChecked = true;
+
+			trayMaker.Update();
+		} });
+	}
+
+	TrayMenu model = { "Model", true, false, false, nullptr, { 
+		new TrayMenu { "Phalanx", true, false, false, changeModel},
+		new TrayMenu { "Ameise", true, false, false, changeModel},
+		new TrayMenu { "Noctiluca", true, false, false, changeModel},
+	}};
+
+	tr.menu.push_back(new TrayMenu { "Phalanx Tray App", false });
+	tr.menu.push_back(new TrayMenu { "-" });
+	tr.menu.push_back(&comPorts);
+	tr.menu.push_back(&model);
+	tr.menu.push_back(new TrayMenu { "Exit", true, false, false, [&](TrayMenu* tm){ 
+		trayMaker.Exit();
+	} });
+
+	if (trayMaker.Initialize(&tr))
+		while(trayMaker.Loop(1)) {	}
+	else
+		std::cout << "initialization failed" << std::endl;
+
+	return 0;
 }

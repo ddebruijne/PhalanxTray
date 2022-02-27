cf8881031216670b802ca72291a35770372cfc7d
Danny de Bruijne <dannydb44@gmail.com>
2022-02-27 10:35:35 +0000

    Model selection persistance

--- source/main.cpp
+++ source/main.cpp
@@ -10,6 +10,7 @@ using namespace PhalanxTray;
 std::vector<TrayMenu*> menu;
 TrayIcon tr = { "icon.png", "icon.ico", "Phalanx Tray App", menu };
 TrayMenu comPorts = { "Serial Port", true, false, false, nullptr };
+TrayMenu status = { "Disconnected", false };
 
 void buildComPortMenu()
 {
@@ -41,14 +42,37 @@ int main()
 	SaveData* sav = saveHandler.GetCurrentSaveData();
 
 	buildComPortMenu();
-
 	TrayMenu model = { "Model", true, false, false, nullptr, { 
-		new TrayMenu { "Phalanx", true, false, false },
-		new TrayMenu { "Ameise", true, false, false },
-		new TrayMenu { "Noctiluca", true, false, false },
+		new TrayMenu { "Phalanx", true, sav->model == EModel::Phalanx, false, [&](TrayMenu* tm){
+			for (TrayMenu* t : model.subMenu)
+				t->isChecked = false;
+			
+			tm->isChecked = true;
+			sav->model = EModel::Phalanx;
+			saveHandler.SaveCurrentData();
+			trayMaker.Update();
+		}},
+		new TrayMenu { "Ameise", true, sav->model == EModel::Ameise, false, [&](TrayMenu* tm){
+			for (TrayMenu* t : model.subMenu)
+				t->isChecked = false;
+			
+			tm->isChecked = true;
+			sav->model = EModel::Ameise;
+			saveHandler.SaveCurrentData();
+			trayMaker.Update();
+		}},
+		new TrayMenu { "Noctiluca", true, sav->model == EModel::Noctiluca, false, [&](TrayMenu* tm){
+			for (TrayMenu* t : model.subMenu)
+				t->isChecked = false;
+			
+			tm->isChecked = true;
+			sav->model = EModel::Noctiluca;
+			saveHandler.SaveCurrentData();
+			trayMaker.Update();
+		}},
 	}};
 
-	tr.menu.push_back(new TrayMenu { "Phalanx Tray App", false });
+	tr.menu.push_back(&status);
 	tr.menu.push_back(new TrayMenu { "-" });
 	tr.menu.push_back(&comPorts);
 	tr.menu.push_back(&model);

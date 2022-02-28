093ef31a95d90f5530bda5d7316346604483ffa6
Danny de Bruijne <dannydb44@gmail.com>
2022-02-28 10:03:26 +0000

    make serial connection

--- source/main.cpp
+++ source/main.cpp
@@ -9,40 +9,85 @@ using namespace PhalanxTray;
 
 std::vector<TrayMenu*> menu;
 TrayIcon tr = { "icon.png", "icon.ico", "Phalanx Tray App", menu };
-TrayMenu comPorts = { "Serial Port", true, false, false, nullptr };
 TrayMenu status = { "Disconnected", false };
+TrayMenu comPorts = { "Serial Port", true, false, false, nullptr };
+TrayMenu model;
+
+serial::Serial serialConn;
+
+void attemptConnect()
+{
+	if (serialConn.isOpen())
+		return;
+
+	SaveData* sav = saveHandler.GetCurrentSaveData();
+
+	serialConn.setPort(std::string(sav->serialport));
+	serialConn.setBaudrate(115200);
+	serialConn.setTimeout(serial::Timeout::simpleTimeout(1000));
+	try
+	{
+		serialConn.open();
+	}
+	catch (serial::SerialException e) 
+	{
+		status.text = "SerialException";
+	}
+	catch (serial::IOException e)
+	{
+		status.text = "IOException";
+	}
+
+	if (!serialConn.isOpen())
+		return;
+
+	status.text = "Connected";	
+	serialConn.write("[MODE]serial\n");
+}
+
+void attemptDisconnect()
+{
+	if (!serialConn.isOpen())
+		return;
+
+	serialConn.write("[MODE]normal\n");
+	serialConn.close();
+	status.text = "Disconnected";	
+}
 
 void buildComPortMenu()
 {
 	for (TrayMenu* t : comPorts.subMenu)
 		delete t;
+
 	comPorts.subMenu.clear();
 
 	SaveData* sav = saveHandler.GetCurrentSaveData();
 	std::vector<serial::PortInfo> devices_found = serial::list_ports();
 	for (serial::PortInfo device : devices_found) 
 	{
-		comPorts.subMenu.push_back(new TrayMenu { device.port + " ("+ device.description + ")", true, strcmp(sav->serialport, device.port.c_str()) == 0, false, [=](TrayMenu* tm){ 
+		comPorts.subMenu.push_back(new TrayMenu { device.port + " ("+ device.description + ")", true, strcmp(sav->serialport, device.port.c_str()) == 0, false, [&, device](TrayMenu* tm){ 
 			for (TrayMenu* t : comPorts.subMenu)
 				t->isChecked = false;
+
+			attemptDisconnect();
 
 			tm->isChecked = true;
 			strcpy(saveHandler.GetCurrentSaveData()->serialport, device.port.c_str());
 			saveHandler.SaveCurrentData();
 
+			attemptConnect();
 			buildComPortMenu();
 			trayMaker.Update();
 		} });
 	}
 }
 
-int main()
+void buildModelMenu()
 {
-	saveHandler.CreateAndLoadSave();
 	SaveData* sav = saveHandler.GetCurrentSaveData();
 
-	buildComPortMenu();
-	TrayMenu model = { "Model", true, false, false, nullptr, { 
+	model = { "Model", true, false, false, nullptr, { 
 		new TrayMenu { "Phalanx", true, sav->model == EModel::Phalanx, false, [&](TrayMenu* tm){
 			for (TrayMenu* t : model.subMenu)
 				t->isChecked = false;
@@ -71,6 +116,15 @@ int main()
 			trayMaker.Update();
 		}},
 	}};
+}
+
+int main()
+{
+	saveHandler.CreateAndLoadSave();
+	SaveData* sav = saveHandler.GetCurrentSaveData();
+
+	buildComPortMenu();
+	buildModelMenu();
 
 	tr.menu.push_back(&status);
 	tr.menu.push_back(new TrayMenu { "-" });
@@ -80,10 +134,16 @@ int main()
 		trayMaker.Exit();
 	} });
 
+
+	if(strlen(sav->serialport) > 0)
+		attemptConnect();
+
 	if (trayMaker.Initialize(&tr))
-		while(trayMaker.Loop(1)) {	}
+		while(trayMaker.Loop(1));
 	else
 		std::cout << "initialization failed" << std::endl;
+
+	attemptDisconnect();
 
 	return 0;
 }
